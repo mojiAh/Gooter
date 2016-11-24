fd3772b18729c05756a7ab63be357091fb5fb4cb
mojtaba <mo.ahmadi@outlook.com>
2016-11-24 13:04:43 +0200

    problem fixed

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,12 @@
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var passport= require('passport');
-var session= require('express-session');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var session = require('express-session');
+var passport = require('passport');
 var api = require('./routes/api');
 var authenticate = require('./routes/authenticate')(passport);
 
@@ -17,10 +17,10 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(session({
-  secret: 'super secret'
+    secret: 'keyboard cat'
 }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -29,30 +29,43 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(passport.initialize());
 app.use(passport.session());
 
-
-//initialize passport
-var initpassport= require('./passport-init');
-initpassport(passport);
-
-app.use('/api', api);
 app.use('/auth', authenticate);
+app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
-// error handler
+//// Initialize Passport
+var initPassport = require('./passport-init');
+initPassport(passport);
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
+
 
 module.exports = app;
--- backend/routes/authenticate.js
+++ backend/routes/authenticate.js
@@ -2,37 +2,37 @@
  * Created by mojtabaahmadi on 23/11/16.
  */
 var express = require('express');
-var router= express.Router();
+var router = express.Router();
 
-module.exports= function (passport) {
+module.exports = function(passport){
 
     //send successful login status back to angular
-    router.get('success', function(req , res){
-       res.send({state: 'success', user: req.user ? req.user :null})
+    router.get('/success', function(req, res){
+       res.send({state: 'success', user: req.user ? req.user : null});
     });
 
     //send failure login status back to angular
-    router.get('/failure',function (req,res) {
-        res.send({state: 'failure', user:null , message: 'Invalid username and password' })
+    router.get('/failure', function(req, res){
+        res.send({state: 'failure', user: null, message: "Invalid username and password"});
     });
 
     //log in
-    router.post('/login', passport.authenticate('login',{
+    router.post('/login', passport.authenticate('login', {
         successRedirect: '/auth/success',
-        failureRedirect: '/auth/failuare'
+        failureRedirect: '/auth/failure'
     }));
 
     //sign up
-    router.post('/signup', passport.authenticate('signup',{
+    router.post('/signup', passport.authenticate('signup', {
         successRedirect: '/auth/success',
         failureRedirect: '/auth/failure'
     }));
 
     //sign out
-    route.get('/signout', function (req ,res) {
+    router.get('/signout', function(req ,res) {
        req.logout();
        res.redirect('/');
     });
 
     return router;
-};
+}
